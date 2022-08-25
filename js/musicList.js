37b9a0fab64b82a12f0ebf7f76f06347433090c9
范光强 <18217610763@163.com>
2022-08-25 12:40:23 +0800

    歌名调整

--- js/musicList.js
+++ js/musicList.js
@@ -1,19 +1,19 @@
 //纯音乐
 const songs1 = [
-    {type: "1", url: "001.mp3", title: "夜的钢琴曲5 - 承利"},
-    {type: "1", url: "003.flac", title: "阿梨粤,R7 - 秒针 (Dj版)"}
+    {type: "1", url: "101.mp3", title: "夜的钢琴曲5 - 承利"},
+    {type: "1", url: "102.flac", title: "阿梨粤,R7 - 秒针 (Dj版)"}
 
 ];
 //古风
 const songs2 = [
-    {type: "1", url: "033.flac", title: "旺仔小乔 - 樱花树下的约定"},
-    {type: "1", url: "012.mp3", title: "买辣椒也用券 - 起风了"}
+    {type: "1", url: "201.flac", title: "旺仔小乔 - 樱花树下的约定"},
+    {type: "1", url: "202.mp3", title: "买辣椒也用券 - 起风了"}
 ];
 //曾经爱听的
 const songs3 = [
-    {type: "1", url: "002.mp3", title: "张芸京 - 偏爱"},
-    {type: "1", url: "019.mp3", title: "红色高跟鞋 ~ 蔡健雅"},
-    {type: "1", url: "040.mp3", title: "风的季节 - Soler"}
+    {type: "1", url: "301.mp3", title: "风的季节 - Soler"},
+    {type: "1", url: "302.mp3", title: "张芸京 - 偏爱"},
+    {type: "1", url: "303.mp3", title: "红色高跟鞋 ~ 蔡健雅"}
 
 ];
 let songs = songs1;
