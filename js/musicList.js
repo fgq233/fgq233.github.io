4af3eb75ba37fda566f5b9ebfd413a92477bda37
范光强 <18217610763@163.com>
2022-08-25 12:33:27 +0800

    歌单调整

--- js/musicList.js
+++ js/musicList.js
@@ -1,52 +1,19 @@
 //纯音乐
 const songs1 = [
     {type: "1", url: "001.mp3", title: "夜的钢琴曲5 - 承利"},
-    {type: "1", url: "027.mp3", title: "Hoaprox - Ngẫu Hứng"},
-    {type: "1", url: "029.mp3", title: "Friendships"},
-    {type: "1", url: "031.mp3", title: "Alan Walker - Faded"},
-    {type: "1", url: "034.mp3", title: "A_Hisa - Caravan"},
-    {type: "1", url: "035.mp3", title: "Windy Hilln"},
+    {type: "1", url: "003.flac", title: "阿梨粤,R7 - 秒针 (Dj版)"}
 
 ];
 //古风
 const songs2 = [
     {type: "1", url: "033.flac", title: "旺仔小乔 - 樱花树下的约定"},
-    {type: "1", url: "012.mp3", title: "买辣椒也用券 - 起风了"},
-    {type: "1", url: "037.mp3", title: "牵丝戏 - 银临、Aki阿杰"},
-    {type: "1", url: "038.mp3", title: "琵琶行 - 奇然、沈谧仁"},
-    {type: "1", url: "039.mp3", title: "吹梦到西洲 -黄诗扶、妖扬"},
-    {type: "1", url: "025.mp3", title: "陈一发 - 童话镇"},
-    {type: "1", url: "020.mp3", title: "河图 - 倾尽天下"},
-    {type: "1", url: "020.mp3", title: "河图 - 风起天阑"},
-    {type: "1", url: "028.mp3", title: "HITA - 赤伶"},
-    {type: "1", url: "009.mp3", title: "双笙 - 我的一个道姑朋友"},
-    {type: "1", url: "010.mp3", title: "任素汐 - 胡广生"},
-    {type: "1", url: "005.flac", title: "一棵小葱 - 狂浪生"},
-    {type: "1", url: "013.mp3", title: "刘珂矣 - 风筝误"},
-    {type: "1", url: "014.mp3", title: "离人愁 - 李袁杰"},
-    {type: "1", url: "015.mp3", title: "兰若词 - 刘亦菲"},
-    {type: "1", url: "018.mp3", title: "后弦、柳岩 - 孙尚香"},
-    {type: "1", url: "022.mp3", title: "花粥 - 归去来兮"},
-    {type: "1", url: "032.mp3", title: "聆音 - 明月天涯"},
+    {type: "1", url: "012.mp3", title: "买辣椒也用券 - 起风了"}
 ];
 //曾经爱听的
 const songs3 = [
     {type: "1", url: "002.mp3", title: "张芸京 - 偏爱"},
-    {type: "1", url: "003.mp3", title: "张杰 - 天下"},
-    {type: "1", url: "004.mp3", title: "缘分一道桥 - 王力宏/谭维维"},
-    {type: "1", url: "006.mp3", title: "天下有情人 - 童珺"},
-    {type: "1", url: "007.mp3", title: "孙子涵 - 唐人"},
-    {type: "1", url: "008.mp3", title: "孙耀威 - 爱的故事"},
-    {type: "1", url: "011.mp3", title: "曲婉婷 - Jar of Love"},
-    {type: "1", url: "016.mp3", title: "胡彦斌 - 江湖再见"},
-    {type: "1", url: "017.mp3", title: "胡歌 - 逍遥叹"},
     {type: "1", url: "019.mp3", title: "红色高跟鞋 ~ 蔡健雅"},
-    {type: "1", url: "023.mp3", title: "广寒宫 - 丸子呦"},
-    {type: "1", url: "024.mp3", title: "范逸臣 - 放生"},
-    {type: "1", url: "026.flac", title: "That Girl"},
-    {type: "1", url: "030.mp3", title: "Alan Walker - Faded - 英雄联盟版"},
-    {type: "1", url: "036.mp3", title: "烬"},
-    {type: "1", url: "040.mp3", title: "风的季节 - Soler"},
+    {type: "1", url: "040.mp3", title: "风的季节 - Soler"}
 
 ];
 let songs = songs1;
