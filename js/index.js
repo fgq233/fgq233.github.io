dc26aa13e921703c21f11c92eed2e5f75131aa40
范光强 <18217610763@163.com>
2023-03-07 18:39:15 +0800

    blog

--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@ function clickBlog() {
     let r = d.getFullYear() + (d.getMonth() + 1) + d.getDate();
     let s = Encrypt(r);
     let ret = prompt('范老师帅不帅', '帅');
-    if(ret !== null && ret === Decrypt(s)) {
+    // if(ret !== null && ret === Decrypt(s)) {
+    //     window.location.href = "https://fgq233.github.io/md/blog";
+    // }
+    if(ret !== null && ret === '帅') {
         window.location.href = "https://fgq233.github.io/md/blog";
     }
 }
