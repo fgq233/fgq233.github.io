2a2d7caf861b69b9005559d1d0dd6043a0804e5f
fgq233 <18217610763@163.com>
2023-03-30 23:09:40 +0800

    music

--- js/musicList.js
+++ js/musicList.js
@@ -1,8 +1,10 @@
 const songs = [
     {url: "https://res.wx.qq.com/voice/getvoice?mediaid=MzI3NjYzODY0Nl8xMDAwMDAzMzc=", title: "买辣椒也用券 - 起风了"},
+    {url: "https://res.wx.qq.com/voice/getvoice?mediaid=MzI3NjYzODY0Nl8xMDAwMDAxODI=", title: "陈一发儿 - 童话镇"},
     {url: "https://res.wx.qq.com/voice/getvoice?mediaid=MzI3NjYzODY0Nl8yMjQ3NDg2MDk4", title: "笑纳 - 花僮"},
     {url: "https://res.wx.qq.com/voice/getvoice?mediaid=MzI3NjYzODY0Nl8xMDAwMDA2MDg=", title: "故人泪 - 麦小兜"},
     {url: "https://res.wx.qq.com/voice/getvoice?mediaid=MzI3NjYzODY0Nl8yMjQ3NDg2NTI0", title: "琵琶行 - 奇然,沈谧仁"},
+    {url: "https://res.wx.qq.com/voice/getvoice?mediaid=MzI3NjYzODY0Nl8yMjQ3NDg3MDYz", title: "红昭愿 - 音阙诗听"},
     {url: "https://rn01-sycdn.kuwo.cn/b2ff87fac90b4dd6a11e2f0b37a1f1b5/6423c924/resource/n3/28/54/323409062.mp3", title: "半壶纱 - 刘珂矣"},
 
 
